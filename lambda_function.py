134c492716fb32fd25389ab5e89cf6fc5008c6e8
omega1644 <nhattan2409cantho@gmail.com>
2023-04-21 11:29:28 +0700

    Adding code to demo the CodeBuild process

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@ import requests
 
 def lambda_handler(event, context):
     url = event["url"]
+    print("Calling requests")
     response = requests.get(url)
     if response.status_code == "200":
         print("Congrats! So proud of you!")
