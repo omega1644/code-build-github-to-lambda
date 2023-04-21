33772180c8e74800acf4f6a241a0049108f8af13
omega1644 <nhattan2409cantho@gmail.com>
2023-04-21 11:34:05 +0700

    Adding code to demo the CodeBuild process

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     print("Calling requests")
     response = requests.get(url)
     if response.status_code == "200":
-        print("Congrats! So proud of you!")
+        print("Congrats! So proud of you !")
         return response.json()
     return {
         "status": f"Request failed with status code : {response.status_code}"
